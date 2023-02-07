96e8a16dfd80bb1a5194570f43c7158e7e2baadb
Matias <116806456+BlauGranaMVB@users.noreply.github.com>
2023-02-07 14:00:16 +0300

    Update osnovi.java

--- osnovi.java
+++ osnovi.java
@@ -31,3 +31,43 @@ public class Main {
         newCar.build();
     }
 }
+
+///////////////////////////////////////////////////////
+public class Main {
+  public static void main(String[] args) {
+        //Yavili miru rytsarya
+        Knight knight = new Knight();
+        //Otpravlyayem yego za printsessoy
+        knight.goAndSaveThePrincess();
+  }
+}
+
+class Knight {
+    private String name = "Sir Thanks-A-Lot";
+    private String weapon = "Long Sword";
+    private Boolean isGoingToSavePrincess = true;
+
+    public static void main(String[] args) {
+    }
+
+    public void goAndSaveThePrincess() {
+        sharpenBlade();
+        getFood();
+        assembleTeam();
+        getHorse();
+        System.out.println("Da idu uzhe...");
+    }
+
+    private void sharpenBlade() {
+        System.out.println("Tochim mech");
+    }
+    private void getFood() {
+        System.out.println("Sobirayem konservy");
+    }
+    private void assembleTeam() {
+        System.out.println("Budim oruzhenostsa");
+    }
+    private void getHorse() {
+        System.out.println("and go go");
+    }
+}
